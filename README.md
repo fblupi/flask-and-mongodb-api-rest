77c83cb20cdb868a59dd9f76a00542e192dd53f6
Francisco Javier Bolívar Lupiáñez <fblupi@correo.ugr.es>
2016-11-06 11:40:01 +0100

    First API REST

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # flask-and-mongodb-api-rest
-My first API REST using Flask and MongoDB
+My first API REST using Flask and MongoDB (http://www.bogotobogo.com/python/MongoDB_PyMongo/python_MongoDB_RESTAPI_with_Flask.php).
+
+## Configuration
+
+* Install MongoDB:
+```
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+```
+* Install PyMongo:
+```
+pip install pymongo
+```
+
+## Running
+
+* Start MongoDB:
+```
+sudo service mongodb Start
+```
+* Run virtualenv:
+```
+virtualenv -p python3 venv
+source venv/bin/activate
+```
+* Run our app:
+```
+pip install Flask-PyMongo
+python mongo.py
+```
---
+++ mongo.py
@@ -0,0 +1,46 @@
+# mongo.py
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'restdb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
+
+app.url_map.strict_slashes = False # Disable redirecting on POST method from /star to /star/
+
+mongo = PyMongo(app)
+
+@app.route('/star', methods=['GET'])
+def get_all_stars():
+    star = mongo.db.stars
+    output = []
+    for s in star.find():
+        output.append({'name' : s['name'], 'distance' : s['distance']})
+    return jsonify({'result' : output})
+
+@app.route('/star/<name>', methods=['GET'])
+def get_one_star(name):
+    star = mongo.db.stars
+    s = star.find_one({'name' : name})
+    if s:
+        output = {'name' : s['name'], 'distance' : s['distance']}
+    else:
+        output = "No such name"
+    return jsonify({'result' : output})
+
+@app.route('/star', methods=['POST'])
+def add_star():
+    star = mongo.db.stars
+    name = request.json['name']
+    distance = request.json['distance']
+    star_id = star.insert({'name': name, 'distance': distance})
+    new_star = star.find_one({'_id': star_id })
+    output = {'name' : new_star['name'], 'distance' : new_star['distance']}
+    return jsonify({'result' : output})
+
+if __name__ == '__main__':
+    app.run(debug=True)
