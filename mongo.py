ac76ff755bbd784263082c1bcf0c4a4c53b2a436
Francisco Javier Bolívar Lupiáñez <fblupi@correo.ugr.es>
2016-11-06 16:08:14 +0100

    Using flask_restful package

--- mongo.py
+++ mongo.py
@@ -1,11 +1,11 @@
 # mongo.py
 
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 app.config['MONGO_DBNAME'] = 'restdb'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
@@ -14,33 +14,35 @@ app.url_map.strict_slashes = False # Disable redirecting on POST method from /st
 
 mongo = PyMongo(app)
 
-@app.route('/star', methods=['GET'])
-def get_all_stars():
-    star = mongo.db.stars
-    output = []
-    for s in star.find():
-        output.append({'name' : s['name'], 'distance' : s['distance']})
-    return jsonify({'result' : output})
+class Star(Resource):
+    def get(self, name):
+        star = mongo.db.stars
+        s = star.find_one({'name' : name})
+        if s:
+            output = {'name' : s['name'], 'distance' : s['distance']}
+        else:
+            output = "No such name"
+        return jsonify({'result' : output})
 
-@app.route('/star/<name>', methods=['GET'])
-def get_one_star(name):
-    star = mongo.db.stars
-    s = star.find_one({'name' : name})
-    if s:
-        output = {'name' : s['name'], 'distance' : s['distance']}
-    else:
-        output = "No such name"
-    return jsonify({'result' : output})
+class StarList(Resource):
+    def get(self):
+        star = mongo.db.stars
+        output = []
+        for s in star.find():
+            output.append({'name' : s['name'], 'distance' : s['distance']})
+        return jsonify({'result' : output})
 
-@app.route('/star', methods=['POST'])
-def add_star():
-    star = mongo.db.stars
-    name = request.json['name']
-    distance = request.json['distance']
-    star_id = star.insert({'name': name, 'distance': distance})
-    new_star = star.find_one({'_id': star_id })
-    output = {'name' : new_star['name'], 'distance' : new_star['distance']}
-    return jsonify({'result' : output})
+    def post(self):
+        star = mongo.db.stars
+        name = request.json['name']
+        distance = request.json['distance']
+        star_id = star.insert({'name': name, 'distance': distance})
+        new_star = star.find_one({'_id': star_id })
+        output = {'name' : new_star['name'], 'distance' : new_star['distance']}
+        return jsonify({'result' : output})
+
+api.add_resource(StarList, '/star')
+api.add_resource(Star, '/star/<string:name>')
 
 if __name__ == '__main__':
     app.run(debug=True)
